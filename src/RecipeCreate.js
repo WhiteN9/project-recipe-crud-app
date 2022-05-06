1ffa4bc6155ce84e278627e48123f2468d200a17
White Nguyen <whitenguyen0909@gmail.com>
2022-05-06 11:20:45 -0600

    added a box and created multiple functions to handle event changes for that box

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,17 +1,50 @@
 import React, { useState } from "react";
 
-function RecipeCreate() {
-
+function RecipeCreate({ makeRecipe }) {
   // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
   // TODO: Add the required input and textarea form elements.
   // TODO: Add the required submit and change handlers
-  
+
+  const initialForm = {
+    name: "",
+    cuisine: "",
+    photo: "",
+    ingredients: "",
+    preparation: "",
+  };
+
+  const [dataForm, setDataForm] = useState({ ...initialForm });
+
+  const handleChange = (evt) => {
+    setDataForm({ ...dataForm, [evt.target.name]: evt.target.value });
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    makeRecipe(dataForm);
+    setDataForm({ ...initialForm });
+  };
+
   return (
-    <form name="create">
+    <form name="create" onSubmit={handleSubmit}>
       <table>
         <tbody>
           <tr>
-            <td></td>
+            <td>
+              <label htmlFor="name"></label>
+              <input
+                id="name"
+                type="text"
+                name="name"
+                value={dataForm.name}
+                onChange={handleChange}
+                required
+                placeholder="Name"
+              ></input>
+            </td>
+            <td>
+
+            </td>
             <td>
               <button type="submit">Create</button>
             </td>
