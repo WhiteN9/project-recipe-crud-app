33aee7a90e2117b37d602d7ea52262ae2507ec2c
White Nguyen <whitenguyen0909@gmail.com>
2022-05-06 14:19:51 -0600

    Updates

---
+++ precache-manifest.9c9234fd5cd9a317535c692429380de8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537ebe3e2c2cdea343b6eafc7458664b",
+    "url": "/project-recipe-crud-app/index.html"
+  },
+  {
+    "revision": "d46f619488c796010978",
+    "url": "/project-recipe-crud-app/static/css/main.86213758.chunk.css"
+  },
+  {
+    "revision": "f394498dd0083a989c2b",
+    "url": "/project-recipe-crud-app/static/js/2.6d741cec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-recipe-crud-app/static/js/2.6d741cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46f619488c796010978",
+    "url": "/project-recipe-crud-app/static/js/main.fbffbf5b.chunk.js"
+  },
+  {
+    "revision": "17e984d3c80bcf9688a7",
+    "url": "/project-recipe-crud-app/static/js/runtime-main.a3cda1c4.js"
+  }
+]);
